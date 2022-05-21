2f452ea182eb7cf92c7bc37a1d36d8ba6cef45f7
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-05-21 22:42:25 +0530

    login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,49 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import Image from "react-bootstrap/Image";
 
 export default function Login() {
-  return(
-    <form>
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <Button >Submit</Button>
+  return (
+    <div className="container">
+      <div className="row row-header">
+        <div className="col-7 col-sm-6 loginHeading">
+          <h1 className="">Supplier Management System</h1>
+          <br></br>
+          <div className="row">
+            <Form>
+              <FormGroup>
+                <Label htmlFor="username" size="lg">
+                  Username
+                </Label>
+                <Input type="text" id="username" name="username" />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password" size="lg">
+                  Password
+                </Label>
+                <Input type="password" id="password" name="password" />
+              </FormGroup>
+              <br></br>
+              <div className="d-grid gap-2">
+                <Button size="lg" type="submit" value="submit" color="primary">
+                  Login
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </div>
+
+        <div class="col-5 col-sm-6 loginImage rounded float-end">
+          <Image
+            src="/assets/images/login.jpg"
+            alt="Login"
+            width="100%"
+          ></Image>
+        </div>
+
       </div>
-    </form>
-  )
+      
+
+    </div>
+  );
 }
