8f86e382f55bd8a07d12093cba4655374adcf78a
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-05-22 14:12:29 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
+import Main from "./pages/main";
 import AddManager from "./components/addsupplymanager";
 import AddSupplier from "./components/addsupplier";
 import AddSupplyRecord from "./components/addsupplyrecord";
@@ -10,7 +11,8 @@ import AddSupplyRecord from "./components/addsupplyrecord";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Main />
   
       <Router>
       <Routes>
@@ -19,6 +21,7 @@ function App() {
       <Route path='/addmanager' element={<AddManager />}></Route>
       <Route path='/addsupplier' element={<AddSupplier />}></Route>
       <Route path='/addsupplyrecord' element={<AddSupplyRecord />}></Route>
+      <Route path='/main' element={<Main/>}></Route>
 
 
       </Routes>
