3b6303bbbd9bb561bcda7fd2d90be8e989e244ea
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-05-23 17:19:46 +0530

    routers adjusted

--- src/App.js
+++ src/App.js
@@ -2,17 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Main from "./pages/main";
-import AddManager from "./components/addsupplymanager";
-import AddSupplier from "./components/addsupplier";
-import AddSupplyRecord from "./components/addsupplyrecord";
-import SupplyRecords from "./pages/supplyrecords";
-import SupplyManagers from "./pages/supplymanagers";
-import Suppliers from "./pages/suppliers";
-
-
-
-
-
 
 function App() {
   return (
@@ -23,14 +12,6 @@ function App() {
       <Routes>
 
       <Route path='/login' element={<Login />}></Route>
-      <Route path='/addmanager' element={<AddManager />}></Route>
-      <Route path='/addsupplier' element={<AddSupplier />}></Route>
-      <Route path='/addsupplyrecord' element={<AddSupplyRecord />}></Route>
-      <Route path='/supplyrecords' element={<SupplyRecords />}></Route>
-      <Route path='/supplymanagers' element={<SupplyManagers />}></Route>
-      <Route path='/suppliers' element={<Suppliers />}></Route>
-
-
       <Route path='/main' element={<Main/>}></Route>
 
 
