61afd05d103c23bfb290b1a765ea1bf5a181277e
GimhanRanasinghe <gimhanravi@gmail.com>
2022-05-24 23:32:45 +0530

    sql added

--- server/src/index.js
+++ server/src/index.js
@@ -9,26 +9,29 @@ db.connect((err) => {
 });
 
 app.get("/", (req, res) => {
+  //   let sql =
+  //     "CREATE TABLE IF NOT EXISTS `Address` ( \
+  //   `aID` int AUTO_INCREMENT,\
+  //   `Line1` varchar(10),\
+  //   `Line2` varchar(50),\
+  //   `City` varchar(20),\
+  //   `District` varchar(20),\
+  //   `Postal_Code` varchar(10),\
+  //   `supplierID` int,\
+  //   PRIMARY KEY (`aID`)\
+  //   ,FOREIGN KEY (supplierID) REFERENCES Supplier(sID) ON DELETE CASCADE \
+  // ) ENGINE=INNODB;";
+  // let sql =
+  //   "CREATE TABLE IF NOT EXISTS `GoodsType` (`gID` int AUTO_INCREMENT,`type` varchar(10),`unit` varchar(10),`photo` blob,PRIMARY KEY (`gID`));";
   let sql =
-    "CREATE TABLE IF NOT EXISTS `Address` ( \
-  `aID` int AUTO_INCREMENT,\
-  `Line1` varchar(10),\
-  `Line2` varchar(50),\
-  `City` varchar(20),\
-  `District` varchar(20),\
-  `Postal_Code` varchar(10),\
-  `supplierID` int,\
-  PRIMARY KEY (`aID`)\
-  ,FOREIGN KEY (supplierID) REFERENCES Supplier(sID) ON DELETE CASCADE \
-) ENGINE=INNODB;";
-// let sql ="CREATE TABLE IF NOT EXISTS `Supplier` (`sID` int NOT NULL AUTO_INCREMENT,`name` varchar(20),`email` varchar(50),`Joined_date` date,`photo` blob,PRIMARY KEY (`sID`)) ENGINE=INNODB;"
- db.query(sql, (err, result) => {
-  if (err) throw err;
-  console.log(result);
-  res.send("Table created");
-});
+    "CREATE TABLE IF NOT EXISTS `SupplyRecord` (`srID` int AUTO_INCREMENT,`sup_ID` int,`unit_Prize` decimal(10,2),`amount` decimal(10,2),`type` int,`recieved_date` Date,PRIMARY KEY (`srID`), FOREIGN KEY (sup_ID) REFERENCES Supplier(sID), FOREIGN KEY (type) REFERENCES GoodsType(gID));";
 
-        
+  // let sql ="CREATE TABLE IF NOT EXISTS `Supplier` (`sID` int NOT NULL AUTO_INCREMENT,`name` varchar(20),`email` varchar(50),`Joined_date` date,`photo` blob,PRIMARY KEY (`sID`)) ENGINE=INNODB;"
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Table created");
+  });
 });
 
 app.listen("3000", () => {
