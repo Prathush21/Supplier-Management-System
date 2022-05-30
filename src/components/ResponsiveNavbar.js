0c29c3020d915fe883762f553785aeb02545164d
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-05-30 15:27:19 +0530

    headercomponent.js is no longer used instead Header.js
    header and navbar is responsive.

---
+++ src/components/ResponsiveNavbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { MDBNavbarToggler, MDBCollapse } from "mdb-react-ui-kit";
+import NavigationBar from "./navbarcomponent";
+
+export default function NavigationComponent() {
+  const [showNavExternal, setShowNavExternal] = useState(false);
+
+  return (
+    <>
+      <div className="d-md-none justify-content-center mt-2">
+        <div className="justify-content-center ">
+          <div className="container mx-auto mt-2">
+            <MDBNavbarToggler
+              type="button"
+              data-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={() => setShowNavExternal(!showNavExternal)}
+            >
+              <span
+                style={{ color: "#fff" }}
+                className="fa fa-bars fa-lg"
+              ></span>
+            </MDBNavbarToggler>
+          </div>
+          <MDBCollapse show={showNavExternal}>
+            <NavigationBar />
+          </MDBCollapse>
+        </div>
+      </div>
+      <div className="d-none d-md-block">
+        <NavigationBar />
+      </div>
+    </>
+  );
+}
--- src/components/maincomponent.js
+++ src/components/maincomponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import Header from "./headercomponent";
-import NavigationBar from "./navbarcomponent";
+import {} from "reactstrap";
 import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import Header from "./Header";
+import NavigationBar from "./navbarcomponent";
+import NavigationComponent from "./ResponsiveNavbar";
 import SupplyManagers from "../components/supplymanagers";
 import Suppliers from "../components/suppliers";
 import AddSupplyRecord from "../components/addsupplyrecord";
@@ -10,9 +12,7 @@ import Goods from "../components/goods";
 import Storage from "../components/storage";
 import EditAccountDetails from "../components/editaccountdetails";
 import EditStorage from "../components/editstorage";
-import {} from "reactstrap";
 import EditSupplier from "../components/editsupplier";
-import {} from "reactstrap";
 import Home from "./homecomponent";
 
 class Main extends Component {
@@ -22,12 +22,13 @@ class Main extends Component {
         <Header />
         <div className="row main">
           <div className="col-12 col-md-2 navigationBar">
-            <NavigationBar />
+            <NavigationComponent />
           </div>
           <div className="col-12 col-md-10">
             <Outlet/>
           </div>
         </div>
+        {/* <div className="container"><Outlet/></div> */}
       </div>
     );
   }
