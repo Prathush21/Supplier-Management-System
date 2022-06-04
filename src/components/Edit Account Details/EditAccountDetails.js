7eb00a7f97941c924d6d7d6c73b47c3dbf60a003
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-06-04 14:57:17 +0530

    edit account details

--- src/components/Edit Account Details/EditAccountDetails.js
+++ src/components/Edit Account Details/EditAccountDetails.js
@@ -1,52 +1,38 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, Table } from "reactstrap";
-import EditUsername from "./EditUsername";
-import EditPassword from "./EditPassword";
 import EditDetails from "./EditDetails";
 import axios from "axios";
 import '../../styles/styles_1.css';
 
-class EditAccountDetails extends Component {
-  constructor(props) {
-    super(props);
+export default function EditAccountDetails () {
 
-    this.toggleModalEditDetails = this.toggleModalEditDetails.bind(this);
-    this.toggleModalEditUsername = this.toggleModalEditUsername.bind(this);
-    this.toggleModalEditPassword = this.toggleModalEditPassword.bind(this);
-    this.state = {
-      editDetails: false,
-      editPassword: false,
-      editUsername: false,
-      userDetails: []
-    };
-  }
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [userDetails, setUserDetails] = useState({
 
-  toggleModalEditUsername() {
-    this.setState({
-      editUsername: !this.state.editUsername,
-    });
-  }
+      name : 'Piumini Kaveesha',
+      email : 'piumini.19@cse.mrt.ac.lk',
+      address : 'Kandy rd, Kurunegala',
+      contact : '0714290810',
+      join_date : '2022-01-01'
 
-  toggleModalEditPassword() {
-    this.setState({
-      editPassword: !this.state.editPassword,
-    });
-  }
+});
 
-  toggleModalEditDetails() {
-    this.setState({
-      editDetails: !this.state.editDetails,
-    });
-  }
+  const setModalIsOpenToTrue = () => {
+    setModalIsOpen(true);
+  };
 
-  render() {
+  const setModalIsOpenToFalse = () => {
+    setModalIsOpen(false);
+  };
+
     
-    axios.get("http://localhost:8087/manager/profile") //Edit Page
-    .then(getUserDetails => {
-      this.setState({userDetails: getUserDetails.data.data[0]});
-    }).catch(err => {
-      console.log(err)
-    })
+    // axios.get("http://localhost:8087/manager/profile") //Edit Page
+    // .then(getUserDetails => {
+    //   // this.setState({userDetails: getUserDetails.data.data[0]});
+    //   setUserDetails(getUserDetails.data.data[0]);
+    // }).catch(err => {
+    //   console.log(err)
+    // })
 
     return (
       <React.Fragment>
@@ -55,29 +41,11 @@ class EditAccountDetails extends Component {
           <br></br>
           <div className="row">
             <div className="col-sm-12 col-md-auto mb-2 align-content-center">
-              <Button color="light" onClick={this.toggleModalEditDetails}>
+              <Button color="light" onClick={setModalIsOpenToTrue}>
                 Edit Account Details
               </Button>
             </div>
 
-            <div className="col-sm-12 col-md-auto mb-2 align-content-left">
-              <Button
-                color="light"
-                onClick={this.toggleModalEditUsername}
-                
-              >
-                Change Username
-              </Button>
-            </div>
-            <div className="col-sm-12 col-md-auto mb-2 align-content-center">
-              <Button
-                color="light"
-                onClick={this.toggleModalEditPassword}
-                
-              >
-                Change Password
-              </Button>
-            </div>
           </div>
           <br></br>
           <Table responsive>
@@ -89,75 +57,49 @@ class EditAccountDetails extends Component {
                 <td colSpan={5}>
                   <b>Name</b>
                 </td>
-                <td>{this.state.userDetails.name}</td>
+                <td>{userDetails.name}</td>
               </tr>
               <br></br>
               <tr>
                 <td colSpan={5}>
                   <b>Email Address</b>
                 </td>
-                <td>{this.state.userDetails.email}</td>
+                <td>{userDetails.email}</td>
               </tr>
               <br></br>
               <tr>
                 <td colSpan={5}>
                   <b>Address</b>
                 </td>
-                <td>{this.state.userDetails.address}</td>
+                <td>{userDetails.address}</td>
               </tr>
               <br></br>
               <tr>
                 <td colSpan={5}>
                   <b>Contact Number</b>
                 </td>
-                <td>{this.state.userDetails.contact}</td>
+                <td>{userDetails.contact}</td>
               </tr>
               <tr>
                 <td colSpan={5}>
                   <b>Joined Date</b>
                 </td>
-                <td>{this.state.userDetails.join_date}</td>
+                <td>{userDetails.join_date}</td>
               </tr>
-
             </tbody>
           </Table>
         </div>
         <Modal
-          isOpen={this.state.editUsername}
-          toggle={this.toggleModalEditUsername}
+          isOpen={modalIsOpen}
         >
-          <ModalHeader toggle={this.toggleModalEditUsername}>
-            <h3>Edit Username </h3>{" "}
+          <ModalHeader close={<Button close onClick={setModalIsOpenToFalse}></Button>}>
+            <h3>Edit Account Details</h3>{" "}
           </ModalHeader>
           <ModalBody>
-            <EditUsername />
-          </ModalBody>
-        </Modal>
-        <Modal
-          isOpen={this.state.editPassword}
-          toggle={this.toggleModalEditPassword}
-        >
-          <ModalHeader toggle={this.toggleModalEditPassword}>
-            <h3>Edit Password </h3>{" "}
-          </ModalHeader>
-          <ModalBody>
-            <EditPassword />
-          </ModalBody>
-        </Modal>
-        <Modal
-          isOpen={this.state.editDetails}
-          toggle={this.toggleModalEditDetails}
-        >
-          <ModalHeader toggle={this.toggleModalEditDetails}>
-            <h3>Edit Account Information</h3>{" "}
-          </ModalHeader>
-          <ModalBody>
-            <EditDetails />
+            <EditDetails userDetails = {userDetails} />
           </ModalBody>
         </Modal>
       </React.Fragment>
     );
   }
-}
 
-export default EditAccountDetails;
--- src/components/Edit Account Details/EditDetails.js
+++ src/components/Edit Account Details/EditDetails.js
@@ -3,22 +3,26 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import '../../styles/styles_1.css';
 
-export default function EditDetails() {
+export default function EditDetails(props) {
+  const initialValues = props.userDetails;
+  initialValues.newpass = ''
+  initialValues.repass = ''
 
-  const [id, setId] = useState('');
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [contactNumber, setContactNumber] = useState('');
+  const [formValues,setformValues] = useState(initialValues)
+  const [isSubmit,setIsSubmit] = useState(false);
+  const [formErrors, setformErrors] = useState({})
+  const [data, setData] = useState(null);
 
-  const userDetails = [
-    {
-      id: 1,
-      name: "Lokesh Kanagaraj",
-      username: "lokesh24",
-      email: "example@gmail.com",
-      contactNumber: "0768582163",
-    },
-  ];
+
+  // const userDetails = [
+  //   {
+  //     id: 1,
+  //     name: "Lokesh Kanagaraj",
+  //     username: "lokesh24",
+  //     email: "example@gmail.com",
+  //     contactNumber: "0768582163",
+  //   },
+  // ];
 
   // axios.get("/manager/update") //Edit Page
 
@@ -32,12 +36,6 @@ export default function EditDetails() {
   const sendData = () => {
     const url = 'http://localhost:8087/manager/update'  //EDIT DETAILS
 
-    const data = {
-      id : id,
-      name : name,
-      email : email,
-      contactNumber : contactNumber
-    }
     axios.post(url, data)
     .then((res) => {
       console.log("response", res)
@@ -46,41 +44,99 @@ export default function EditDetails() {
     })
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformValues({...formValues, [name]:value});
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setformErrors(validate(formValues));
+    setIsSubmit(true);  
+    setData(formValues);
+  }
+  
   useEffect(() => {
-    setId(userDetails[0].id);
-    setName(userDetails[0].name);
-    setEmail(userDetails[0].email);
-    setContactNumber(userDetails[0].contactNumber);
-  });
+    
+    if(Object.keys(formErrors).length === 0 && isSubmit){
+      sendData()
+    }
+  }, [formErrors]);
+  
+  const validate = (values) => {
+    const errors = {}
+    const reContact = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    const reEmail =  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    
+    if ((values.name).length < 3){
+      errors.name = 'Name must be at least 3 characters';
+    }
+    if(!reEmail.test(values.email)){
+      errors.email = 'Invalid Email Address';
+    }
+    
+    if(!reContact.test(values.contact)){
+      errors.contact = 'Invalid Contact Number'
+    }
+    if((values.newpass).length < 8){
+      errors.newpass = 'Password must be at least 8 characters';
+    }
+    if(!(values.newpass === values.repass)){
+      errors.repass = 'Password did not match !'
+    }
+    return errors;
+  } 
+  
 
   return (
     <div className="Container-fluid shadow-2-strong">
 
-      <Form className="form">
+      <Form className="form" onSubmit={handleSubmit}>
+      <FormGroup>
+          <Label for="name">Name</Label>
+          <Input type="text" name="name" id="name" value={formValues.name}
+          onChange={handleChange} invalid={(formErrors.name === 'Name must be at least 3 characters')}/>
+          <p class="fst-italic fw-bolder" style={{color:'#f93154'}}>{formErrors.name}</p>
+        </FormGroup>
+
+
         <FormGroup>
-          <Label for="Name">Name</Label>
-          <Input type="text" name="Name" id="Name"
-          placeholder={name}
-          onChange={(e) => setName(e.target.value)}/>
+          <Label for="email">Email Address</Label>
+          <Input type="email" name="email" id="email" value={formValues.email}
+          onChange={handleChange} invalid={(formErrors.email === 'Invalid Email Address')} />
+          <p class="fst-italic fw-bolder" style={{color:'#f93154'}}>{formErrors.email}</p>
         </FormGroup>
 
         <FormGroup>
-          <Label for="email">Email Address</Label> 
-          <Input type="text" name="email" id="email" 
-          placeholder={email}
-          onChange={(e) => setEmail(e.target.value)}/>
+          <Label for="text">Address</Label> 
+          <Input type="text" name="address" id="address" 
+          value={formValues.address}
+          onChange={handleChange}/>
         </FormGroup>
 
         <FormGroup>
-          <Label for="contactno">Contact number</Label>
-          <Input type="text"  name="contactno" id="contactno" 
-          placeholder={contactNumber}
-          onChange={(e) => setContactNumber(e.target.value)}/>
+          <Label for="contact">Contact Number</Label>
+          <Input type="text" name="contact" id="contact" value={formValues.contact} 
+          onChange={handleChange} invalid={(formErrors.contact === 'Invalid Contact Number')}/>
+          <p class="fst-italic fw-bolder" style={{color:'#f93154'}}>{formErrors.contact}</p>
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="newpass">New Password</Label>
+          <Input type="password"  name="newpass" id="newpass" 
+         onChange={handleChange} invalid={(formErrors.newpass === 'Password must be at least 8 characters')}/>
+         <p class="fst-italic fw-bolder" style={{color:'#f93154'}}>{formErrors.newpass}</p>
+        </FormGroup>
+        <FormGroup>
+          <Label for="repass">Re-enter new password</Label>
+          <Input type="password"  name="repass" id="repass" 
+          onChange={handleChange} invalid={(formErrors.repass === 'Password did not match !')}/>
+          <p class="fst-italic fw-bolder" style={{color:'#f93154'}}>{formErrors.repass}</p>
         </FormGroup>
 
      
-        <Button color="primary"
-        onClick={sendData}> Update </Button>
+        <Button color="primary" type="submit"> Update </Button>
       </Form>
     </div>
   );
